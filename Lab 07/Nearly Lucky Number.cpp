c1fb25349ac1554a86bcd3230388852151b1c5b4
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2022-10-17 23:50:03 +0600

    Add files via upload

---
+++ Lab 07/Nearly Lucky Number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   string str;
+   cin >> str;
+   int incr=0, i;
+
+   for(i=0; i<str.size();i++){
+     if(str[i]=='4' || str[i]=='7'){
+         incr++;
+     }
+   }
+   if(incr==4 || incr==7){
+      cout << "YES" << endl;
+   } else {
+      cout << "NO" << endl;
+   }
+   return 0;
+}
